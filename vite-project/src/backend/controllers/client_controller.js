bb20f155a7c1df0cc7ae292976ee5ac50fd0379a
Gandino1984 <andinogerman@gmail.com>
2024-11-08 10:27:54 +0100

    crud client controller

--- vite-project/src/backend/controllers/client_controller.js
+++ vite-project/src/backend/controllers/client_controller.js
@@ -6,12 +6,18 @@ async function getAById(id) {
     return `Get all clients by id. Id = ${id}`;
 }
 
-async function create() {
-    return "Create new client"
+async function create(clientData) {
+    return `Create client: Data = ${id_client} , ${name_client} , ${pass_client} , ${location_client}`
 }   
 
 async function update(id, clientData) {
     const {id_client, name_client, pass_client, location_client } = clientData;
     return `Update client: Data = ${id_client} , ${name_client} , ${pass_client} , ${location_client}`
 }
+
+async function removeById(id) {
+    return `Delete client by id. Id = ${id}`
+}
+
+export { getAll, getAById, create, update, removeById }
 
