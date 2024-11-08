b63621a51a042cab5bbfc4cfde8351d31d20210d
Gandino1984 <andinogerman@gmail.com>
2024-11-08 07:53:08 +0100

    changes package.json

--- vite-project/src/backend/routes/router.js
+++ vite-project/src/backend/routes/router.js
@@ -6,9 +6,5 @@ router.get("/", (req, res) => {
     res.send("GET request to main project route")  
   })
 
-router.get("/search", (req, res) => {
-   const query = req.query.q;
-   res.send(`GET request to search route with query: ${query}`)
-  })
 
 export default router;
